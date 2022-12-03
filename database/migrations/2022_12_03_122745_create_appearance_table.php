6a6cf72f200513a3ae73e1055e68c3104b2e86c0
kniki5 <kissnikolett515@gmail.com>
2022-12-03 13:46:42 +0100

    appearance table

---
+++ database/migrations/2022_12_03_122745_create_appearance_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appearance', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->date('datum');
+            $table->int('not_appear');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appearance');
+    }
+};
