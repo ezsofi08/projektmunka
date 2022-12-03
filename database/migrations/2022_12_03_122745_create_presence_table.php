ed15c8f5deb1652c3fa802e63ba25b009e78a546
kniki5 <kissnikolett515@gmail.com>
2022-12-03 13:57:30 +0100

    appearance -> presence table update

--- database/migrations/2022_12_03_122745_create_presence_table.php
+++ database/migrations/2022_12_03_122745_create_presence_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('appearance', function (Blueprint $table) {
+        Schema::create('presence', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
             $table->date('datum');
-            $table->int('not_appear');
+            $table->integer('not_appear');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('appearance');
+        Schema::dropIfExists('presence');
     }
 };
