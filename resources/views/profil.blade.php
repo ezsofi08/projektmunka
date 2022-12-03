3a56fd838c5386a825af5347e7a922dda3dd34c1
sanyi24 <nagysanyi96@gmail.com>
2022-12-03 18:39:21 +0100

    Basic display of picked up appointments by the user @ (WILL CHANGE IN FUTURE COMMIT)

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -29,12 +29,12 @@ label{
             <p>Email cím: {{$user->email}}</p>
             <p>Telefonszám: {{$user->phone_number}}</p>
             <p>Tajszám: {{$user->TAJ}}</p>
-            <p>Születési dátum: {{$user->birthday}}</p> 
+            <p>Születési dátum: {{$user->birthday}}</p>
         </div>
-        
-                        
+
+
       </div>
-      
+
       <div class="col-lg-6 col-md-8 mb-3 mb-md-0">
       <h3 class="mb-3">Kórelőzmény:</h3>
 
@@ -47,11 +47,11 @@ label{
       <th scope="col">Kezelőorvos</th>
     </tr>
   </thead>
-  
+
   <tbody>
   @foreach ($documents as $document)
     <tr>
-      
+
      <td>{{ $document -> date}}</td>
      <td>{{ $document -> treatment}}</td>
      <td>{{ $document -> doctor_first_name}} {{ $document -> doctor_second_name}}</td>
@@ -60,6 +60,26 @@ label{
   </tbody>
 </table>
 <a href="{{route('profil_pdf')}}">Részletes kórelőzmény letöltése</a>
+{{-- Copy pasteltem a kórelőzményes táblázatot, ide beszúrtam egy próba-megjelenítést
+csak azért hogy lássam működik-e. Az adatok lekérését a 98. sortól látjátok --}}
+<h3 class="mb-3">Időpontok:</h3>
+<table class="table">
+  <thead>
+  <tr>
+      <th scope="col">Kezelőorvos</th>
+      <th scope="col">Mikor</th>
+  </tr>
+  </thead>
+
+  <tbody>
+  @foreach ($appointments as $appointment)
+      <tr>
+          <td>{{ \App\Models\Doctors::all()->find($appointment->doctor_id)->name }}</td>
+          <td>{{ $appointment->end_at}}</td>
+      </tr>
+  @endforeach
+  </tbody>
+</table>
 </div>
 </div>
 </div>
