88207c9e0380036542afa4461af9a882240acebc
kniki5 <kissnikolett515@gmail.com>
2022-12-03 13:08:01 +0100

    Admin home design update

--- resources/views/admin/adminhome.blade.php
+++ resources/views/admin/adminhome.blade.php
@@ -19,11 +19,11 @@
 
 </style>
 
-<div class="container_2">
+<div class="container_2 justify-content-center d-flex list-inline text-center">
  <div class="list-group" id="but">
-  <a href="/admin/new_appointment" class="list-group-item list-group-item-action">Új időpont feltöltése</a>
-  <a href="/admin/valami" class="list-group-item list-group-item-action">Következő időpontom</a>
-  <a href="#" class="list-group-item list-group-item-action">Kórelőzmény letöltése</a>
+  <a href="/admin/new_appointment" class="list-group-item-info list-group-item list-group-item-action">Új időpont feltöltése</a>
+  <a href="/admin/valami" class="list-group-item-info list-group-item list-group-item-action">Következő időpontom</a>
+  <a href="#" class="list-group-item-info list-group-item list-group-item-action">Kórelőzmény letöltése</a>
 
 </div>
 </div>  
