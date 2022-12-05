5817669f29d94981ff745cf9b9e2c6e5883004e1
kniki5 <kissnikolett515@gmail.com>
2022-12-05 16:02:14 +0100

    question blade update

--- resources/views/pdf/questions.blade.php
+++ resources/views/pdf/questions.blade.php
@@ -1,12 +1,34 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+<meta charset="UTF-8">
+
+</head>
+<style>
+.pdf_title{
+    
+    align: center;
+    text-align: center;
+    font-weight: bold;
+    font-size: 30;
+    /*font-family: 'Exo', sans-serif;*/
+}
+
+.pdf_content{
+    margin: 20px;
+    font-size: 18;
+}
+
+</style>
 @foreach ($questions as $qu)
 
-    <h1 class="pdf_title">Kérdőív</h1>
+    <h1 class="pdf_title">Űrlap</h1>
     <br>
     <br>
-    <p class="pdf_content"> {{ $qu -> reason}}</p>
-    <p class="pdf_content"> {{ $qu-> complain}}</p>
-    <p class="pdf_content">{{ $qu-> time}}</p>
-    <p class="pdf_content">{{ $qu-> braces}}</p>
-    <p class="pdf_content">{{ $qu-> medicine}}</p>
+    <p class="pdf_content"> Látogatás oka: {{ $qu -> reason}}</p>
+    <p class="pdf_content"> Panaszok: {{ $qu-> complain}}</p>
+    <p class="pdf_content"> Panaszok fennállásának ideje: {{ $qu-> time}}</p>
+    <p class="pdf_content"> Fogszabályzó: {{ $qu-> braces}}</p>
+    <p class="pdf_content"> Gyógyszerallergia: {{ $qu-> medicine}}</p>
    </div>
 @endforeach
