c9cfbad63d9cfb247eada61d5cdca4046a458bae
sanyi24 <nagysanyi96@gmail.com>
2022-12-05 17:14:04 +0100

    Fix display reserved appointments

--- app/Http/Controllers/AdminBookController.php
+++ app/Http/Controllers/AdminBookController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Appointment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -18,12 +19,12 @@ class AdminBookController  extends Controller
     $user=Auth::user();
     //$appointment = DB::table('appointments')->where('doctor_id', $user->id)->where('user_id','<>',null)->get();
     //$appointment = DB::table('appointments')->where('doctor_id', $user->id);//->where('user_id','<>',null)->orderBy('end_at','ASC');
-    
 
     $appointment = DB::table('appointments')
              ->join('users', 'users.id', '=', 'appointments.user_id')
              ->select('appointments.*', 'users.firstname', 'users.secondname')
              ->where('user_id', '<>', null)
+             ->where('appointments.doctor_id','=', $user->id)
              ->orderBy('end_at')
              ->get();
 
